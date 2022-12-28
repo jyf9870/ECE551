2d00e3ecce14b477c8822830f6e59a41ccd1dbac
jyf9870 <80262336+jyf9870@users.noreply.github.com>
2022-12-28 00:28:14 -0500

    Add files via upload

---
+++ 029_power_rec/power.c
@@ -0,0 +1,11 @@
+
+#include "power.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y) {
+  if (y == 0)
+    return 1;
+  return x * power(x, y - 1);
+}
---
+++ 029_power_rec/power.h
@@ -0,0 +1,6 @@
+#ifndef __POWER_H__
+#define __POWER_H__
+
+unsigned power(unsigned x, unsigned y);
+
+#endif
